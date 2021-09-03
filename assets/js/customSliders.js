4279c48fc6b046a2f3cfd6d763de742b617e3845
Md Rezaul Karim Shatno <63625974+mdrkshanto@users.noreply.github.com>
2021-09-03 15:07:53 +0600

    xzoom
    Added xzoom plugin.

--- assets/js/customSliders.js
+++ assets/js/customSliders.js
@@ -72,4 +72,46 @@ $(document).ready(function(){
         arrows: true,
         draggable: false
     });
+
+
+
+    $('.xzoomLens, .xzoomgalleryLens').xzoom({position: 'lens', lensShape: 'circle', sourceClass: 'xzoom-hidden'});
+        var isTouchSupported = 'ontouchstart' in window;
+
+        if (isTouchSupported) {
+            //If touch device
+            $('.xzoomLens').each(function(){
+                var xzoom = $(this).data('xzoom');
+                xzoom.eventunbind();
+            });
+            
+            $('.xzoomLens').each(function() {
+                var xzoom = $(this).data('xzoom');
+                $(this).hammer().on("tap", function(event) {
+                    event.pageX = event.gesture.center.pageX;
+                    event.pageY = event.gesture.center.pageY;
+                    var s = 1, ls;
+    
+                    xzoom.eventmove = function(element) {
+                        element.hammer().on('drag', function(event) {
+                            event.pageX = event.gesture.center.pageX;
+                            event.pageY = event.gesture.center.pageY;
+                            xzoom.movezoom(event);
+                            event.gesture.preventDefault();
+                        });
+                    }
+    
+                    xzoom.eventleave = function(element) {
+                        element.hammer().on('tap', function(event) {
+                            xzoom.closezoom();
+                        });
+                    }
+                    xzoom.openzoom(event);
+                });
+            });
+
+        }
+
+
+
 });
